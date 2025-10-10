0a927cb9b4a54f6aab813ff14095d48500579ef9
laurhakob <laurhakob@gmail.com>
2025-10-11 03:22:44 +0400

    agents page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -10,6 +10,7 @@ import {
   MAX_PAGE_SIZE,
   MIN_PAGE_SIZE,
 } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
@@ -22,11 +23,13 @@ export const agentsRouter = createTRPCRouter({
         })
         .from(agents)
         .where(
-          and(
-            eq(agents.id, input.id)
-            // eq(agents.userId, ctx.auth.user.id),
-          )
+          and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))
         );
+
+
+        if (!existingAgent) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+    }
 
       return existingAgent;
     }),
@@ -69,7 +72,7 @@ export const agentsRouter = createTRPCRouter({
         );
 
       const totalPages = Math.ceil(total.count / pageSize);
- 
+
       return {
         items: data,
         total: total.count,
