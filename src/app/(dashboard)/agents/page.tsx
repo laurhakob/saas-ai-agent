c2ba4e9ba4fbfbf1fec030d981ecc9021a313ebe
laurhakob <laurhakob@gmail.com>
2025-10-10 14:54:20 +0400

    agents filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -3,6 +3,8 @@ import {
   AgentsViewError,
   AgentsViewLoading,
 } from "@/modules/agents/ui/views/agents-view";
+import type { SearchParams } from "nuqs";
+
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
@@ -11,23 +13,33 @@ import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-hea
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const Page = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
 
-   const session = await auth.api.getSession({
-      headers: await headers(),
-    });
-  
-    if (!session) {
-      redirect("/sign-in");
-    }
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
   return (
     <>
-    <AgentsListHeader />
+      <AgentsListHeader />
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<AgentsViewLoading />}>
           <ErrorBoundary fallback={<AgentsViewError />}>
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,39 +1,28 @@
-// export const AgentsListHeader = () => {
-//     return ( 
-//         <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
-//             <div></div>
-//         </div>
-//      );
-// }
- 
-
-
-
 "use client";
 
 import { useState } from "react";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 
-// import { DEFAULT_PAGE } from "@/constants";
+import { DEFAULT_PAGE } from "@/constants";
 import { Button } from "@/components/ui/button";
-// import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 import { NewAgentDialog } from "./new-agent-dialog";
-// import { AgentsSearchFilter } from "./agents-search-filter";
-// import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { AgentsSearchFilter } from "./agents-search-filter";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
 
 export const AgentsListHeader = () => {
-//   const [filters, setFilters] = useAgentsFilters();
-   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [filters, setFilters] = useAgentsFilters();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-//   const isAnyFilterModified = !!filters.search;
+  const isAnyFilterModified = !!filters.search;
 
-//   const onClearFilters = () => {
-//     setFilters({
-//       search: "",
-//       page: DEFAULT_PAGE,
-//     });
-//   }
+  const onClearFilters = () => {
+    setFilters({
+      search: "",
+      page: DEFAULT_PAGE,
+    });
+  }
 
   return (
     <>
@@ -46,7 +35,7 @@ export const AgentsListHeader = () => {
             New Agent
           </Button>
         </div>
-        {/* <ScrollArea>
+        <ScrollArea>
           <div className="flex items-center gap-x-2 p-1">
             <AgentsSearchFilter />
             {isAnyFilterModified && (
@@ -57,7 +46,7 @@ export const AgentsListHeader = () => {
             )}
           </div>
           <ScrollBar orientation="horizontal" />
-        </ScrollArea> */}
+        </ScrollArea>
       </div>
     </>
   );
