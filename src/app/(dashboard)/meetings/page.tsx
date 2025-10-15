3556c40fd048e9cd8617ec21b581af07a75b520b
laurhakob <laurhakob@gmail.com>
2025-10-16 01:40:41 +0400

    connecting agents stream openAI

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,9 +1,6 @@
-// import { Suspense } from "react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import type { SearchParams } from "nuqs/server";
-// import { ErrorBoundary } from "react-error-boundary";
-// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import {
   MeetingsView,
@@ -16,53 +13,13 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { auth } from "@/lib/auth";
-// import { getQueryClient, trpc } from "@/trpc/server";
 
 import { loadSearchParams } from "@/modules/meetings/params";
 import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header";
-// import {
-//   MeetingsView,
-//   MeetingsViewError,
-//   MeetingsViewLoading
-// } from "@/modules/meetings/ui/views/meetings-view";
 
 interface Props {
   searchParams: Promise<SearchParams>;
 }
-
-// const Page = async ({ searchParams }: Props) => {
-//   const filters = await loadSearchParams(searchParams);
-
-//   const session = await auth.api.getSession({
-//     headers: await headers(),
-//   });
-
-//   if (!session) {
-//     redirect("/sign-in");
-//   }
-
-//   const queryClient = getQueryClient();
-//   void queryClient.prefetchQuery(
-//     trpc.meetings.getMany.queryOptions({
-//       ...filters,
-//     })
-//   );
-
-//   return (
-//     <>
-//       <MeetingsListHeader />
-//       <HydrationBoundary state={dehydrate(queryClient)}>
-//         <Suspense fallback={<MeetingsViewLoading />}>
-//           <ErrorBoundary fallback={<MeetingsViewError />}>
-//             <MeetingsView />
-//           </ErrorBoundary>
-//         </Suspense>
-//       </HydrationBoundary>
-//     </>
-//   );
-// };
-
-// export default Page;
 
 const Page = async ({ searchParams }: Props) => {
   const filters = await loadSearchParams(searchParams);
